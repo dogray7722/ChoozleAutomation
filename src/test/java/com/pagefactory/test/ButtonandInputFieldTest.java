d3b0276e62942b3e138ee4940bcee27c2ff05ffe
davidgray <david.gray61@du.edu>
2018-01-03 16:03:42 -0700

    Add TestNG.xml configuration settings to pom.xml.

--- src/test/java/com/pagefactory/test/ButtonandInputFieldTest.java
+++ src/test/java/com/pagefactory/test/ButtonandInputFieldTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -14,15 +13,15 @@ public class ButtonandInputFieldTest {
     WebDriver driver;
 
     @Test
-    @Parameters("browser")
-    public void multiBrowserTest(String browserName)
+    @Parameters({ "browser" })
+    public void multiBrowserTest(String browser)
     {
-        if (browserName.equalsIgnoreCase("firefox"))
+        if (browser.equalsIgnoreCase("firefox"))
         {
             driver = new FirefoxDriver();
             System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/geckodriver");
         }
-        else if (browserName.equalsIgnoreCase("chrome"))
+        else if (browser.equalsIgnoreCase("chrome"))
         {
             driver = new ChromeDriver();
             System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver");
@@ -35,33 +34,32 @@ public class ButtonandInputFieldTest {
     }
 
 
-//    @Test
-//
-//    public void ButtonandInputField(String browserName)
-//    {
-//
-//        WebDriver driver=Helper.startBrowser("http://choozle.vm/users/login");
-//
-//        LoginLogOut login_page= PageFactory.initElements(driver, LoginLogOut.class);
-//
-//        login_page.login_logout("devs@choozle.com", "choozle123");
-//
-//        driver.quit();
-//
-//    }
-//
-//    @Test
-//    public void MyOrg()
-//    {
-//        WebDriver driver=Helper.startBrowser("http://choozle.vm/users/login");
-//
-//        LoginLogOut login_page = PageFactory.initElements(driver, LoginLogOut.class);
-//
-//        login_page.login_choozle("dufuscookie@mailinator.com", "choozle123");
-//
-//        driver.quit();
-//
-//    }
+    @Test
+    public void ButtonandInputField()
+    {
+
+        WebDriver driver=Helper.startBrowser("http://choozle.vm/users/login");
+
+        LoginLogOut login_page= PageFactory.initElements(driver, LoginLogOut.class);
+
+        login_page.login_logout("devs@choozle.com", "choozle123");
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void MyOrg()
+    {
+        WebDriver driver=Helper.startBrowser("http://choozle.vm/users/login");
+
+        LoginLogOut login_page = PageFactory.initElements(driver, LoginLogOut.class);
+
+        login_page.login_choozle("dufuscookie@mailinator.com", "choozle123");
+
+        driver.quit();
+
+    }
 
 
 }
--- src/test/java/com/pagefactory/test/Helper.java
+++ src/test/java/com/pagefactory/test/Helper.java
@@ -1,19 +1,12 @@
 package com.pagefactory.test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Helper {
 
     static WebDriver driver;
 
     public static WebDriver startBrowser(String url) {
-            WebDriver driver=new ChromeDriver();
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver");
-
-//        WebDriver driver = new FirefoxDriver();
-//        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/geckodriver");
 
         driver.manage().window().maximize();
         driver.get(url);
