d0b0e04abebfa4753b5612fda0e10dd36f6ddbca
davidgray <david.gray61@du.edu>
2018-01-03 14:13:24 -0700

    Refactor to use TestNG annotations.

---
+++ src/test/java/com/pagefactory/test/ButtonandInputFieldTest.java
@@ -0,0 +1,67 @@
+package com.pagefactory.test;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ButtonandInputFieldTest {
+
+    WebDriver driver;
+
+    @Test
+    @Parameters("browser")
+    public void multiBrowserTest(String browserName)
+    {
+        if (browserName.equalsIgnoreCase("firefox"))
+        {
+            driver = new FirefoxDriver();
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/geckodriver");
+        }
+        else if (browserName.equalsIgnoreCase("chrome"))
+        {
+            driver = new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver");
+        }
+
+        driver.manage().window().maximize();
+        driver.get("http://choozle.vm/users/login");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+
+//    @Test
+//
+//    public void ButtonandInputField(String browserName)
+//    {
+//
+//        WebDriver driver=Helper.startBrowser("http://choozle.vm/users/login");
+//
+//        LoginLogOut login_page= PageFactory.initElements(driver, LoginLogOut.class);
+//
+//        login_page.login_logout("devs@choozle.com", "choozle123");
+//
+//        driver.quit();
+//
+//    }
+//
+//    @Test
+//    public void MyOrg()
+//    {
+//        WebDriver driver=Helper.startBrowser("http://choozle.vm/users/login");
+//
+//        LoginLogOut login_page = PageFactory.initElements(driver, LoginLogOut.class);
+//
+//        login_page.login_choozle("dufuscookie@mailinator.com", "choozle123");
+//
+//        driver.quit();
+//
+//    }
+
+
+}
